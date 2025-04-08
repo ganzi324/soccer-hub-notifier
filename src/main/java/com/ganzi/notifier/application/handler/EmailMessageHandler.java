bba7e9cce2bf52ea889976fef8c4006609a071fb
ganzi324 <ganzi324@gmail.com>
2025-04-08 16:32:28 +0900

    Feat: EmailNotification 메시지 핸들러 구현

---
+++ src/main/java/com/ganzi/notifier/application/handler/EmailMessageHandler.java
@@ -0,0 +1,24 @@
+package com.ganzi.notifier.application.handler;
+
+import com.ganzi.notifier.application.service.EmailService;
+import com.ganzi.notifier.notification.domain.EmailNotification;
+import com.ganzi.notifier.notification.domain.Notification;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EmailMessageHandler implements NotificationMessageHandler<EmailNotification> {
+
+    private final EmailService emailService;
+
+    @Override
+    public Class<? extends Notification> getSupportClass() {
+        return EmailNotification.class;
+    }
+
+    @Override
+    public void handle(EmailNotification notification) {
+        emailService.send(notification.getTarget().to().getFirst(), notification.getContent().subject(), notification.getContent().body());
+    }
+}
