efa0edb48c417a0266daf436f90a79296b06748c
ganzi324 <ganzi324@gmail.com>
2025-04-08 16:33:20 +0900

    Feat: Notification 메시지 핸들러 매퍼 구현

---
+++ src/main/java/com/ganzi/notifier/application/handler/NotificationHandlerMapper.java
@@ -0,0 +1,22 @@
+package com.ganzi.notifier.application.handler;
+
+import com.ganzi.notifier.notification.domain.Notification;
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Getter
+@Component
+public class NotificationHandlerMapper {
+
+    private final Map<Class<? extends Notification>, NotificationMessageHandler<?>> handlerMap;
+
+    public NotificationHandlerMapper(List<NotificationMessageHandler<?>> handlers) {
+        this.handlerMap = handlers.stream()
+                .collect(Collectors.toMap(NotificationMessageHandler::getSupportClass, Function.identity()));
+    }
+}
