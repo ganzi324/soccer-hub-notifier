cfd3b56d9626456064857673ade89292b224da68
ganzi324 <ganzi324@gmail.com>
2025-04-09 21:30:19 +0900

    Refactor: EmailService 에러 처리 변경

--- src/main/java/com/ganzi/notifier/application/service/EmailService.java
+++ src/main/java/com/ganzi/notifier/application/service/EmailService.java
@@ -12,16 +12,12 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
 
-    public void send(String to, String subject, String body) {
-        try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setTo(to);
-            message.setSubject(subject);
-            message.setText(body);
+    public void send(String to, String subject, String body) throws MailException {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
 
-            mailSender.send(message);
-        } catch(MailException e) {
-
-        }
+        mailSender.send(message);
     }
 }
