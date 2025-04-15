23d89ef1563e78c8bf523b1eb2e520fa3bd42db4
ganzi324 <ganzi324@gmail.com>
2025-04-15 16:40:51 +0900

    Feat: NotificationMessage 컨슈머 메시지 타입 변경, 영속성 적용

--- src/main/java/com/ganzi/notifier/infra/messaging/NotificationConsumer.java
+++ src/main/java/com/ganzi/notifier/infra/messaging/NotificationConsumer.java
@@ -1,10 +1,11 @@
 package com.ganzi.notifier.infra.messaging;
 
+import com.ganzi.notifier.application.exception.NotificationSendFailException;
 import com.ganzi.notifier.application.handler.NotificationHandlerMapper;
 import com.ganzi.notifier.application.handler.NotificationMessageHandler;
 import com.ganzi.notifier.common.config.RabbitMQConfig;
-import com.ganzi.notifier.application.exception.NotificationSendFailException;
 import com.ganzi.notifier.domain.Notification;
+import com.ganzi.notifier.infra.persistence.NotificationPersistenceService;
 import com.rabbitmq.client.Channel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
@@ -20,29 +21,44 @@ import java.util.Map;
 @Component
 public class NotificationConsumer {
 
-    private final Map<Class<? extends Notification>, NotificationMessageHandler<?>> handlers;
+    private final Map<Class<? extends NotificationMessage>, NotificationMessageHandler<?>> handlers;
     private final RabbitTemplate rabbitTemplate;
+    private final NotificationMessageMapperFactory mapperFactory;
+    private final NotificationPersistenceService persistenceService;
 
-    public NotificationConsumer(NotificationHandlerMapper handlerMapper, RabbitTemplate rabbitTemplate) {
+    public NotificationConsumer(NotificationHandlerMapper handlerMapper, RabbitTemplate rabbitTemplate, NotificationMessageMapperFactory mapperFactory, NotificationPersistenceService persistenceService) {
         this.handlers = handlerMapper.getHandlerMap();
         this.rabbitTemplate = rabbitTemplate;
+        this.mapperFactory = mapperFactory;
+        this.persistenceService = persistenceService;
     }
 
     @RabbitListener(queues = RabbitMQConfig.MAIN_QUEUE)
-    public void receive(Notification notification, Message message, Channel channel) throws IOException {
+    public void receive(NotificationMessage notificationMessage, Message message, Channel channel) throws IOException {
+        Notification notification = null;
+
         try {
-            Class<? extends Notification> clazz = notification.getClass();
+            Class<? extends NotificationMessage> clazz = notificationMessage.getClass();
             NotificationMessageHandler<?> handler = handlers.get(clazz);
 
             if (handler == null) {
                 throw new IllegalArgumentException("No handler for notification class: " + clazz.getSimpleName());
             }
 
+            notification = persistenceService.findById(notificationMessage.getId())
+                    .orElseGet(() -> convertMessage(notificationMessage));
+
             handleNotification(handler, notification);
+            saveHistory(notification);
             channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
         } catch (NotificationSendFailException exception) {
             log.error("Failed to handle notification", exception);
             long retryCount = getRetryCount(message);
+
+            if (notification != null) {
+                notification.retry();
+                saveHistory(notification);
+            }
 
             if (retryCount == 1) {
                 sendToRetryQueue(RabbitMQConfig.ROUTING_KEY_RETRY_2, message);
@@ -60,6 +76,7 @@ public class NotificationConsumer {
     private <T extends Notification> void handleNotification(NotificationMessageHandler<T> handler, Notification notification) {
         try {
             handler.handle((T) notification);
+            notification.markAsSuccess();
         } catch (NotificationSendFailException e) {
             throw e;
         } catch (Exception e) {
@@ -81,9 +98,20 @@ public class NotificationConsumer {
 
     private void sendToRetryQueue(String routingKey, Message message) {
         rabbitTemplate.send(RabbitMQConfig.MAIN_EXCHANGE, routingKey, message);
+
     }
 
     private void sendToDLQ(Message message) {
         rabbitTemplate.send(RabbitMQConfig.DLX_EXCHANGE, RabbitMQConfig.ROUTING_KEY_DLQ, message);
+    }
+
+    private Notification convertMessage(NotificationMessage notificationMessage) {
+        NotificationMessageMapper<Notification, ? extends NotificationMessage> mapper = mapperFactory.getMapper(notificationMessage.getClass());
+
+        return ((NotificationMessageMapper<Notification, NotificationMessage>)mapper).toDomain(notificationMessage);
+    }
+
+    private void saveHistory(Notification notification) {
+        persistenceService.save(notification);
     }
 }
