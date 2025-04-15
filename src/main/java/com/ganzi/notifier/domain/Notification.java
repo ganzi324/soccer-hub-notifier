da26c39f139c0db0890eaff6af382076a98c7475
ganzi324 <ganzi324@gmail.com>
2025-04-15 15:48:58 +0900

    Feat: Notification 추상 클래스로 변경, 상태 변경, 재시도 횟수 메소드 추가

--- src/main/java/com/ganzi/notifier/domain/Notification.java
+++ src/main/java/com/ganzi/notifier/domain/Notification.java
@@ -1,10 +1,61 @@
 package com.ganzi.notifier.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
 import java.util.UUID;
 
-public interface Notification {
-    UUID getId();
-    NotificationType getType();
-    Target getTarget();
-    Content getContent();
+@NoArgsConstructor
+@Getter
+public abstract class Notification {
+
+    private static final int MAX_RETRIES = 2;
+
+    private UUID id;
+    private NotificationStatus status;
+    private int retryCount;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    public Notification(UUID id, NotificationStatus status, int retryCount, Instant createdAt, Instant updatedAt) {
+        this.id = id;
+        this.status = status;
+        this.retryCount = retryCount;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Notification(UUID id, NotificationStatus status) {
+        this.id = id;
+        this.status = status;
+        this.retryCount = 0;
+        this.createdAt = Instant.now();
+        this.updatedAt = Instant.now();
+    }
+
+    public void markAsSuccess() {
+        this.status = NotificationStatus.SUCCESS;
+    }
+
+    public void markAsFailed() {
+        this.status = NotificationStatus.FAILED;
+    }
+
+    public void markAsPending() {
+        this.status = NotificationStatus.PENDING;
+    }
+
+    public void retry() {
+        if (retryCount >= MAX_RETRIES) {
+            this.status = NotificationStatus.FAILED;
+        } else {
+            this.retryCount++;
+            this.status = NotificationStatus.RETRY;
+        }
+    }
+
+    public abstract NotificationType getType();
+    public abstract Target getTarget();
+    public abstract Content getContent();
 }
---
+++ src/main/java/com/ganzi/notifier/domain/NotificationStatus.java
@@ -0,0 +1,5 @@
+package com.ganzi.notifier.domain;
+
+public enum NotificationStatus {
+    PENDING, SUCCESS, FAILED, RETRY
+}
