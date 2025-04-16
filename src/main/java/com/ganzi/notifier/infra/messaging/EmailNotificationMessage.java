3c0220a3aee138bdf276d4d2c0cb692853226fef
ganzi324 <ganzi324@gmail.com>
2025-04-16 17:09:20 +0900

    Refactor: EmailNotificationMessage 모든 속성 생성자 추가

--- src/main/java/com/ganzi/notifier/infra/messaging/EmailNotificationMessage.java
+++ src/main/java/com/ganzi/notifier/infra/messaging/EmailNotificationMessage.java
@@ -1,10 +1,12 @@
 package com.ganzi.notifier.infra.messaging;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.UUID;
 
 @Getter
+@AllArgsConstructor
 public class EmailNotificationMessage implements NotificationMessage {
     private UUID id;
     private EmailTargetMessage target;
