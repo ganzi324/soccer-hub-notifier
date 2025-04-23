16129c79bc951b07d036390072ce2531e3758f70
ganzi324 <ganzi324@gmail.com>
2025-04-23 18:20:43 +0900

    Fix: EmailNotificationMessage 기본 생성자 누락

--- src/main/java/com/ganzi/notifier/infra/messaging/EmailNotificationMessage.java
+++ src/main/java/com/ganzi/notifier/infra/messaging/EmailNotificationMessage.java
@@ -2,11 +2,13 @@ package com.ganzi.notifier.infra.messaging;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class EmailNotificationMessage implements NotificationMessage {
     private UUID id;
     private EmailTargetMessage target;
